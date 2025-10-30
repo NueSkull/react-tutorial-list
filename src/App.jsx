1c35c2a96c8534d15c79a8448e078eb6bd2bb102
NueSkull <markjtravers@hotmail.com>
2025-10-30 13:38:47 +0000

    ToDo list created

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Task from "./components/Task"
+import {useState} from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [toDos,setToDos] = useState(["Wake up","Grab a brush", "Put a little make-up", "Hide the scars to fade away the shake-up", "Why'd you leave the keys upon the table?"]);
+
+  const handleDelete = (deletedTask) => {
+        setToDos((currentToDos) => {        
+        return currentToDos.filter((task) => {
+          return task !== deletedTask;
+        })
+
+        })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My To-Do List</h1>
+      <p>Welcome to the to-do list! Make note of your daily tasks, and tick them off as you go!</p>
+      <p> An inline javascript is wrapped inside curly braces, so 2 + 2 inside curly braces is {2 + 2}</p>
+      <br/>
+      <p>We can make a list using an array!</p>
+      <ul>
+        {toDos.map((task) => {
+          return <Task key={task} task={task} handleDelete={handleDelete}/>
+        })}
+      </ul>
     </>
   )
 }
 
-export default App
+export default App;
